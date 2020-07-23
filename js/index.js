80b944eebdee7a4fdf4db5adcc273213965ac99a
Marcos <marcozzymandias@gmail.com>
2020-07-23 12:24:11 -0300

    first day

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const root = document.querySelector('#root')
+
+function newElement(tag, content) {
+    const title = `<${tag}> ${content} </${tag}>`
+
+    root.insertAdjacentHTML('beforeend', title)
+}
+
+
+newElement('h4', 'Eita')
+newElement('h1', 'A, vá')
